597bf179bae683aa73291f73c64c69131bb85e8e
takahiro.tominaga <tt.300607@gmail.com>
2023-01-29 21:33:10 +0900

    add: cobra init (#1)

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "sgi-cli",
+	Short: "About sakamichi group information",
+	Long:  `SGI is a CLI libracy about sakamichi group information.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/android-project-46group/sgi-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
